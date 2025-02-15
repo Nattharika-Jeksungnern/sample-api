6341498ea1a9e23b3a852c5aa9d3a5d02b9d9851
Nattharika <Nattharika25kata@gmail.com>
2025-02-15 16:04:17 +0700

    back to start

--- tests/test_plus.py
+++ tests/test_plus.py
@@ -1,40 +1,13 @@
 import unittest
 from app import app
-import json
 
 class TestAPI(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
     def test_plus(self):
-        print("Requesting /plus/5/6")
         response = self.app.get('/plus/5/6')
-
-        # Check status code
-        print("Response Status Code:", response.status_code)
-        self.assertEqual(response.status_code, 200)
-
-        # Print response data
-        print("Response Data:", response.data.decode('utf-8'))
-
-        # Decode response
-        json_data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(json_data['plus'], 11)
-
-    def test_plus_float(self):
-        print("Requesting /plus/8.4/4")
-        response = self.app.get('/plus/8.4/4')
-
-        # Check status code
-        print("Response Status Code:", response.status_code)
-        self.assertEqual(response.status_code, 200)
-
-        # Print response data
-        print("Response Data:", response.data.decode('utf-8'))
-
-        # Decode response
-        json_data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual(json_data['plus'], 12.4)
+        self.assertEqual(response.data.decode('utf-8'), '11')
 
 if __name__ == '__main__':
     unittest.main()
